28d6bb93a81a0c7c6c2fa67b4f59f2e3f5379a34
Panagiotis <Panagiotis@PANOS-PC>
2016-10-30 12:26:03 +0200

    -

---
+++ OmdbAPISampleApp/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OmdbAPISampleApp.Models;
+
+namespace OmdbAPISampleApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+        }
+    }
+}
