69a11edca951b814590aca7d222bc4a96e933f6b
Panagiotis <Panagiotis@PANOS-PC>
2016-10-30 12:56:39 +0200

    -

--- OmdbAPISampleApp/App_Start/BundleConfig.cs
+++ OmdbAPISampleApp/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace OmdbAPISampleApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/FiveColumns.css")
+                      );
         }
     }
 }
